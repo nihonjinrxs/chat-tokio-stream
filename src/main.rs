f17362a9594266373fe5f2c5ef49d7d118da7740
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2022-11-02 20:51:37 -0500

    Implement a multi-client, multi-message echo server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,30 @@
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::TcpListener
+};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let listener: TcpListener = TcpListener::bind("localhost:8080").await.unwrap();
+
+    loop {
+        let (mut socket, _addr) = listener.accept().await.unwrap();
+
+        tokio::spawn(async move{
+            let (reader, mut writer) = socket.split();
+
+            let mut buf_reader = BufReader::new(reader);
+            let mut line = String::new();
+
+            loop {
+                let bytes_read: usize = buf_reader.read_line(&mut line).await.unwrap();
+                if bytes_read == 0 {
+                    break;
+                }
+
+                writer.write_all(line.as_bytes()).await.unwrap();
+                line.clear();
+            }
+        });
+    }
 }
