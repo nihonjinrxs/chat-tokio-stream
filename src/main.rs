bd499c85005002628cc15f884265fa2de5bfd426
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2022-11-04 01:03:15 -0500

    Expand echo server into a multi-client chat server

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
-    net::TcpListener
+    net::TcpListener,
+    sync::broadcast,
 };
 
 #[tokio::main]
 async fn main() {
     let listener: TcpListener = TcpListener::bind("localhost:8080").await.unwrap();
 
+    let (tx, _rx) = broadcast::channel(10);
+
     loop {
-        let (mut socket, _addr) = listener.accept().await.unwrap();
+        let (mut socket, addr) = listener.accept().await.unwrap();
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
 
         tokio::spawn(async move{
             let (reader, mut writer) = socket.split();
@@ -17,13 +22,21 @@ async fn main() {
             let mut line = String::new();
 
             loop {
-                let bytes_read: usize = buf_reader.read_line(&mut line).await.unwrap();
-                if bytes_read == 0 {
-                    break;
+                tokio::select! {
+                    result = buf_reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
+                            break;
+                        }
+                        tx.send((line.clone(), addr)).unwrap();
+                        line.clear();
+                    }
+                    result = rx.recv() => {
+                        let (msg, other_addr) = result.unwrap();
+                        if addr != other_addr {
+                            writer.write_all(msg.as_bytes()).await.unwrap();
+                        }
+                    }
                 }
-
-                writer.write_all(line.as_bytes()).await.unwrap();
-                line.clear();
             }
         });
     }
